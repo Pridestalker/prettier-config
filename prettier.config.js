0ab4f59105b7b856ce43dc74a5a6f55c014d82e7
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-02-12 11:08:23 +0100

    Added fields
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   "$schema": "http://json.schemastore.org/prettierrc",
+  'useTabs': false,
   'singleQuote': true,
-  'trailingComma': 'all',
+  'trailingComma': 'es5',
   'semi': true,
-  'tabWidth': 2
+  'tabWidth': 2,
+  'printWidth': 120,
+  'quoteProps': 'consistent',
+  'bracketSpacing': true,
+  'vueIndentScriptAndStyle': false,
 };
